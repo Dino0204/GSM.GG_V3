09506664cafdaf3eb2b4dbe0fafba6809870bc5b
Dino0204 <s24020@gsm.hs.kr>
2025-04-01 11:38:36 +0900

    Feat: 헤더

---
+++ src/shared/ui/header/header.tsx
@@ -0,0 +1,33 @@
+import { useNavigate } from "react-router-dom"
+import { Button } from "../button/button"
+
+export const Header = () => {
+  const go = useNavigate()
+
+  return (
+    <header className="h-[6.25rem]">
+      <section className="flex items-center bg-base-300 p-2">
+        <img
+          src="./public/logo/GSM.GG Logo.svg" alt="logo"
+          className="ml-2 cursor-pointer" onClick={() => go("/")}
+        />
+      </section>
+      <section className="flex items-center bg-primary-200 p-2 justify-between text-sm text-white font-bold ">
+        <ul className="flex items-center gap-4 ">
+          <li className="cursor-pointer" onClick={() => go("/champions")}>챔피언 분석</li>
+          <li className="cursor-pointer" onClick={() => go("/colors")}>색상</li>
+        </ul>
+        <div className="flex items-center gap-2">
+          <Button variant="primary" label="로그인" onClick={() => go("/login")} />
+          <div className="flex items-center gap-2 cursor-pointer" onClick={() => go("/profile")}>
+            프로필
+            <img className="h-8" src="./public/profile.svg" alt="profile" />
+          </div>
+        </div>
+      </section>
+    </header>
+  )
+}
+
+
+
