ae32ff8e2dd65df5df8551aa4d80854a042b2f09
Dino0204 <s24020@gsm.hs.kr>
2025-04-01 11:37:07 +0900

    Feat: Router

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,16 +1,26 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
+
 import Main from '../pages/main/ui/main'
-import SignUp from '../pages/signup/ui/signup'
 import Champions from '../pages/champions/ui/champions'
+import Colors from '../pages/colors/ui/colors'
+
+import { AuthLayout } from '../widgets/auth/ui/authLayout'
+import SignUp from '../pages/signup/ui/signup'
+import Login from '../pages/login/ui/login'
 
 export default function Router() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/champions" element={<Champions />} />
+        <Route path="/" element={<Main />} >
+          <Route path="champions" element={<Champions />} />
+          <Route path="colors" element={<Colors />} />
+        </Route>
+        <Route element={<AuthLayout />}>
+          <Route path="signup" element={<SignUp />} />
+          <Route path="login" element={<Login />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
