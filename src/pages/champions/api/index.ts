0b7cf3624fb80db256631c911a751792c2b08fd9
Dino0204 <s24020@gsm.hs.kr>
2025-04-03 02:35:29 +0900

    Feat: 챔피언 전체목록 페이지

---
+++ src/pages/champions/api/index.ts
@@ -0,0 +1,15 @@
+import { API } from "../../../app/types/api";
+import { RIOT_API, DDRAGON, VER, KR, AS } from "../../../app/consts";
+
+/** 모든 챔피언 정보 획득 */
+export const getChampData = async () => {
+  const res = await API<any>(`${DDRAGON}/${VER}/data/ko_KR/champion.json`, {
+    method: "GET",
+  });
+  return res;
+};
+
+/** 챔피언 프로필 이미지 취득 */
+export const getChampProfile = (champ_id: string) => {
+  return `${DDRAGON}/${VER}/img/champion/${champ_id}.png`;
+};
--- src/pages/champions/ui/champions.tsx
+++ src/pages/champions/ui/champions.tsx
@@ -1,9 +1,26 @@
-import { ChampCard } from "../../../widgets/champions/ui/champCard"
+import { ChampCard } from "../../../widgets/card/ui/champCard"
+import { useQuery } from "@tanstack/react-query";
+import { getChampData, getChampProfile } from "../api";
 
 export default function Champions() {
+  const { data, error, isFetching } = useQuery({
+    queryKey: ['champions'],
+    queryFn: getChampData,
+    staleTime: 1000 * 10
+  })
+
+  const champions = Object.values(data?.data?.data || {})
+
   return (
-    <div>
-      <ChampCard />
+    <div className="flex flex-col justify-center items-center text-white font-medium">
+      {champions.map((champion) =>
+        <ChampCard
+          key={champion?.key}
+          name={champion?.name}
+          splashImage={getChampProfile(champion?.id)}
+          id={champion?.id}
+        />
+      )}
     </div>
   )
 }
