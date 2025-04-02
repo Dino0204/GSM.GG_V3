db63bc377bee591d432d5d85c1b20c804ad63e1c
Dino0204 <s24020@gsm.hs.kr>
2025-04-03 02:32:18 +0900

    Feat: ReactQueryProvider

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import RQProvider from './providers/RQProvider'
 
 import Main from '../pages/main/ui/main'
 import Champions from '../pages/champions/ui/champions'
@@ -7,22 +8,26 @@ import Colors from '../pages/colors/ui/colors'
 import { AuthLayout } from '../widgets/auth/ui/authLayout'
 import SignUp from '../pages/signup/ui/signup'
 import Login from '../pages/login/ui/login'
+import Detail from '../pages/detail/ui/detail'
 
 export default function Router() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} >
-          <Route path="champions" element={<Champions />} />
-          <Route path="colors" element={<Colors />} />
-        </Route>
-        <Route element={<AuthLayout />}>
-          <Route path="signup" element={<SignUp />} />
-          <Route path="login" element={<Login />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <RQProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} >
+            <Route path="champions" element={<Champions />} />
+            <Route path="champions/:id" element={<Detail />} />
+            <Route path="colors" element={<Colors />} />
+          </Route>
+          <Route element={<AuthLayout />}>
+            <Route path="signup" element={<SignUp />} />
+            <Route path="login" element={<Login />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </RQProvider>
   )
 }
 
