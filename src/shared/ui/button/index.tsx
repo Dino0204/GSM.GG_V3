cc50845dff6b655a47e3026fca68498db884fd09
Dino0204 <s24020@gsm.hs.kr>
2025-06-23 11:32:34 +0900

    feat: shared Component

--- src/shared/ui/button/index.tsx
+++ src/shared/ui/button/index.tsx
@@ -3,23 +3,18 @@ import { useMemo } from "react";
 interface ButtonProps {
   label: string;
   isActive?: boolean;
-  variant: "primary" | "secondary";
-  type?: "button" | "submit"
+  variant: "blue";
   onClick?: () => void;
 }
 
-export const Button = ({ label, isActive = false, variant, type = "button", onClick }: ButtonProps) => {
+export const Button = ({ label, isActive = false, variant, onClick }: ButtonProps) => {
   const buttonStyle = useMemo(() => {
-    const primary = isActive ? "bg-blue-500 text-white" : "bg-gray-200 text-gray-500";
-    const secondary = isActive ? "border-blue-400 text-blue-400" : "border-gray-300 text-gray-800";
 
-    return `${variant == "primary" ? primary : secondary}`;
   }, [isActive, variant]);
 
   return (
     <button
-      className={`flex py-2 px-3 border justify-center items-center self-stretch rounded-[0.75rem] font-bold cursor-pointer ${buttonStyle}`}
-      disabled={!isActive && type == "submit"}
+      className={`flex py-[1.31rem] px-[1rem] font-extrabold text-[2rem] text-white justify-center items-center rounded-[0.625rem] bg-primary-200 cursor-pointer ${buttonStyle}`}
       onClick={onClick}
     >
       {label}
---
+++ src/shared/ui/item/index.tsx
@@ -0,0 +1,12 @@
+interface ItemProps {
+  type: "skill" | "rune"
+}
+
+
+export const Item = ({ type }: ItemProps) => {
+  return (
+    <div className={`w-[1.5rem] h-[1.5rem] bg-base-200 ${type == "skill" ? "rounded-xs" : "rounded-full"} `}>
+
+    </div>
+  )
+}
